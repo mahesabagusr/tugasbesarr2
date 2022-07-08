623961244eece353fb89236bf715e618d6fd1f24
mahesabagusr <mahesabagusraditya1@gmail.com>
2022-07-08 21:24:36 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function startlog() {
+  var net1 = document.getElementById("username").value;
+  var net2 = document.getElementById("password").value;
+  if (net1 == net2);
+  {
+    alert("login successfull");
+  }
+}
